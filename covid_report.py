706fb5fa90935ea7eac7132dad4f130357dfc590
carlospjobgether <carlos.perez@jobgether.com>
2021-11-26 21:23:07 -0500

    numero de contagios en colombia

--- covid_report.py
+++ covid_report.py
@@ -10,3 +10,6 @@ import pandas as pd
 url = 'covid_22_noviembre.csv'
 data = pd.read_csv(url)
 
+# Número de casos de Contagiados en el País
+num_casos = data.shape[0]
+print(f"El numero de casos en Colombia es: {num_casos}")
