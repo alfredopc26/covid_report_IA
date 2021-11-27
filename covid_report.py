3837f43075e41191ec3f35ff38d55fdc4ffdd9e9
carlospjobgether <carlos.perez@jobgether.com>
2021-11-26 22:13:52 -0500

    Lista de municipios afectados

--- covid_report.py
+++ covid_report.py
@@ -13,3 +13,16 @@ data = pd.read_csv(url)
 # Número de casos de Contagiados en el País
 num_casos = data.shape[0]
 print(f"El numero de casos en Colombia es: {num_casos}")
+
+# Número de Municipios Afectados
+mun_afectados = data['Nombre municipio'].unique().shape[0]
+print(f"El numero de municipios afectados son: {mun_afectados}")
+
+# Liste los municipios afectados (sin repetirlos)
+def print_array(array):    
+    
+    for x in array:
+        print(x)
+        
+        
+print_array(data['Nombre municipio'].unique())
